4a8f2457445ed4ff872603762292b602223e0183
Aaron Gibran <aarongibran13@gmail.com>
2024-08-23 23:28:40 -0300

    feat:Doctor Settings

---
+++ src/main/java/com/webService/appointmentScheduling/entities/doctor.java
@@ -0,0 +1,11 @@
+package com.webService.appointmentScheduling.entities;
+
+public class doctor {
+    private Long id;
+    private String name;
+    private String specialization;
+    private String login;
+    private String password;
+    private String role;
+
+}
---
+++ src/main/java/com/webService/appointmentScheduling/repositories/doctorRepository.java
@@ -0,0 +1,4 @@
+package com.webService.appointmentScheduling.repositories;
+
+public interface doctorRepository {
+}
