81d7f31233f4d98f330a1361c7da08981f00c1ab
Aaron Gibran <aarongibran13@gmail.com>
2024-08-24 01:09:31 -0300

    fix:Doctor Settings/User Settings

--- src/main/java/com/webService/appointmentScheduling/entities/User.java
+++ src/main/java/com/webService/appointmentScheduling/entities/User.java
@@ -1,13 +1,82 @@
 package com.webService.appointmentScheduling.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
-public class User {
+@Table(name = "Users")
+public class User implements Serializable {
+    private  static final  long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String login;
     private String password;
     private String role;
+
+    public User(Long id, String nome, String login, String password, String role) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+    public  User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
