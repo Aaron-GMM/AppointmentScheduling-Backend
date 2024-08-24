1b11278a52e7971896e654a70a0b0b2a821c472b
Aaron Gibran <aarongibran13@gmail.com>
2024-08-24 01:09:31 -0300

    fix:Doctor Settings/User Settings

--- src/main/java/com/webService/appointmentScheduling/entities/doctor.java
+++ src/main/java/com/webService/appointmentScheduling/entities/doctor.java
@@ -1,14 +1,94 @@
 package com.webService.appointmentScheduling.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
-public class doctor {
-    private Long id;
+@Table(name = "doctors")
+public class doctor implements Serializable {
+    private  static final  long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
     private String name;
     private String specialization;
     private String login;
     private String password;
     private String role;
 
+    public doctor(String role, Long id, String name, String specialization, String login, String password) {
+        this.role = role;
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+        this.login = login;
+        this.password = password;
+    }
+    public doctor(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        doctor doctor = (doctor) o;
+        return Objects.equals(id, doctor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
