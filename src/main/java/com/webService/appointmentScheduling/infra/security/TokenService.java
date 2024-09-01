cfe2e8deb74d368dc532d8dd299bdffbdf38436b
Aaron Gibran <aarongibran13@gmail.com>
2024-09-01 13:35:15 -0300

    refactor:folderStructure

--- src/main/java/com/webService/appointmentScheduling/infra/security/TokenService.java
+++ src/main/java/com/webService/appointmentScheduling/infra/security/TokenService.java
@@ -4,7 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.webService.appointmentScheduling.entities.user.User;
+import com.webService.appointmentScheduling.DTO.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
