b9695971d5410f52e9e4fa82c026e492e5e46af6
aarongm <aarogibran13@gmail.com>
2024-11-03 13:06:13 -0300

    feat:Doctors-DTOs/DoctorService-Controller

--- src/main/java/com/webService/appointmentScheduling/infra/security/SecurityConfigurations.java
+++ src/main/java/com/webService/appointmentScheduling/infra/security/SecurityConfigurations.java
@@ -29,7 +29,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize-> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/register/doctor").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/doctor/register").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET,"/appointment").authenticated()
                         .requestMatchers(HttpMethod.GET, "/doctors/all").authenticated()
                         .anyRequest().permitAll()
--- src/main/java/com/webService/appointmentScheduling/service/doctorService.java
+++ src/main/java/com/webService/appointmentScheduling/service/doctorService.java
@@ -1,5 +1,7 @@
 package com.webService.appointmentScheduling.service;
 
+import com.webService.appointmentScheduling.DTO.doctor.DoctorRequestDTO;
+import com.webService.appointmentScheduling.DTO.doctor.DoctorResponseDTO;
 import com.webService.appointmentScheduling.repositories.doctorRepository;
 import com.webService.appointmentScheduling.entities.Doctor;
 import com.webService.appointmentScheduling.service.exceptions.DatabaseException;
@@ -7,8 +9,10 @@ import com.webService.appointmentScheduling.service.exceptions.ResourceNotFoundE
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class doctorService {
@@ -16,43 +20,63 @@ public class doctorService {
     @Autowired
     private doctorRepository repository;
 
-    public List<Doctor> findAll(){
-        return repository.findAll();
+    public List<DoctorResponseDTO> findAll() {
+        List<Doctor> doctors = repository.findAll();
+        return doctors.stream()
+                .map(this::convertToResponseDTO)
+                .collect(Collectors.toList());
+    }
+    public Doctor findByIdEntity(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
-
-    public Doctor findById(Long id){
-        Optional<Doctor> obj = repository.findById(id);
-        return obj.orElseThrow(()->new ResourceNotFoundException(id));
-
+    public DoctorResponseDTO findById(Long id) {
+        Doctor doctor = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+        return convertToResponseDTO(doctor);
     }
 
-    public Doctor insert(Doctor doctor){
-        return  repository.save(doctor);
+    public DoctorResponseDTO insert(DoctorRequestDTO doctorRequestDTO) {
+        Doctor doctor = convertToEntity(doctorRequestDTO);
+        doctor = repository.save(doctor);
+        return convertToResponseDTO(doctor);
     }
 
-    public Doctor udpadte(Long id, Doctor newDoctor){
+    public DoctorResponseDTO update(Long id, DoctorRequestDTO doctorRequestDTO) {
         try {
-            Doctor entity  = repository.getReferenceById(id);
-            updateData(entity, newDoctor);
-            return repository.save(entity);
-        }catch (EmptyResultDataAccessException e){
+            Doctor entity = repository.getReferenceById(id);
+            updateData(entity, doctorRequestDTO);
+            entity = repository.save(entity);
+            return convertToResponseDTO(entity);
+        } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
         }
     }
-    public void updateData(Doctor entity, Doctor docObj){
-        entity.setName(docObj.getName());
-        entity.setCpf(docObj.getCpf());
-        entity.setDataNascimento(docObj.getDataNascimento());
-        entity.setTell(docObj.getTell());
-        entity.setSpecialization(docObj.getSpecialization());
-    }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         try {
             repository.deleteById(id);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             throw new DatabaseException(e.getMessage());
         }
-        }
     }
+
+    private void updateData(Doctor entity, DoctorRequestDTO doctorRequestDTO) {
+        entity.setName(doctorRequestDTO.getName());
+        entity.setCpf(doctorRequestDTO.getCpf());
+        entity.setDataNascimento(doctorRequestDTO.getDataNascimento());
+        entity.setTell(doctorRequestDTO.getTell());
+        entity.setSpecialization(doctorRequestDTO.getSpecialization());
+    }
+
+    private Doctor convertToEntity(DoctorRequestDTO doctorRequestDTO) {
+        return new Doctor(null, doctorRequestDTO.getName(), doctorRequestDTO.getTell(),
+                doctorRequestDTO.getCpf(), doctorRequestDTO.getSpecialization(), doctorRequestDTO.getDataNascimento());
+    }
+
+    private DoctorResponseDTO convertToResponseDTO(Doctor doctor) {
+        return new DoctorResponseDTO(doctor.getId(), doctor.getName(), doctor.getTell(),
+                doctor.getCpf(), doctor.getSpecialization(), doctor.getDataNascimento());
+    }
+}
