0b2a7f04b5a2d12025a9bbfef8a2e630a363ccb4
aarongm <aarogibran13@gmail.com>
2024-11-09 14:38:34 -0300

    feat:DoctorControllerUpdateDelete

--- src/main/java/com/webService/appointmentScheduling/web/controller/doctorController.java
+++ src/main/java/com/webService/appointmentScheduling/web/controller/doctorController.java
@@ -2,6 +2,7 @@ package com.webService.appointmentScheduling.web.controller;
 import com.webService.appointmentScheduling.DTO.doctor.DoctorRequestDTO;
 import com.webService.appointmentScheduling.DTO.doctor.DoctorResponseDTO;
 import com.webService.appointmentScheduling.service.doctorService;
+import com.webService.appointmentScheduling.service.exceptions.DatabaseException;
 import com.webService.appointmentScheduling.service.exceptions.DoctorCreationException;
 import com.webService.appointmentScheduling.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,6 +57,30 @@ public class doctorController {
             return ResponseEntity.ok(doctorResponseDTO);
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/id/{id}")
+    public ResponseEntity<DoctorResponseDTO> updateDoctor(@PathVariable Long id, @RequestBody DoctorRequestDTO requestDTO){
+        DoctorResponseDTO  responseDTO = service.update(id, requestDTO);
+        if(responseDTO != null){
+            return  ResponseEntity.ok(responseDTO);
+        }else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<?> deletDoctor(@PathVariable Long id){
+        try {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (ResourceNotFoundException erroNtF) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erroNtF.getMessage());
+        }catch (DatabaseException erroDb){
+            return  ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Erro de disponibilidade do Bancod de Dados:"+erroDb.getMessage());
+        }catch (Exception errouUnknow){
+            return ResponseEntity.status(520).body("Erro Desconhecido:"+errouUnknow.getMessage());
         }
     }
 
