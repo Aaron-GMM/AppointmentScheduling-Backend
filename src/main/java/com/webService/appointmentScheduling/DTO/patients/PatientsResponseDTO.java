0de7fec582683a15ede0b4a6d5e442b5abcb07d6
Aaron Gibran <aarongibran13@gmail.com>
2025-04-22 09:02:18 -0300

    feat: class patientsDTO

---
+++ src/main/java/com/webService/appointmentScheduling/DTO/patients/PatientsResponseDTO.java
@@ -0,0 +1,72 @@
+package com.webService.appointmentScheduling.DTO.patients;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class PatientsResponseDTO  implements Serializable {
+    private  static  final  long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String name;
+    private String cpf;
+    private String tell;
+    private Date dataNascimento;
+
+    public PatientsResponseDTO(Long id, String name, String cpf, String tell, Date dataNascimento) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.tell = tell;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public PatientsResponseDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTell() {
+        return tell;
+    }
+
+    public void setTell(String tell) {
+        this.tell = tell;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+
+}
